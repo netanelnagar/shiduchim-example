ab418f0863d0870426959be44dc70c2f0051c4c5
netanelnagar <netanelnagar1234@gmail.com>
2026-02-24 18:42:12 +0200

    feat: Removed the `start-basic-auth` project and introduced new static HTML example pages

--- start-basic-auth/src/routes/example1.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/example1')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/example1"!</div>
-}
--- start-basic-auth/src/routes/example3.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/example3')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/example3"!</div>
-}
